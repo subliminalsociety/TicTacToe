7f5ca63975a54b5b69d4898b4317cb2882c398cf
Chris Castillo <68573334+subliminalsociety@users.noreply.github.com>
2022-10-12 00:19:15 -0500

    Initial commit

---
+++ Form_TicTacToe/MainForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Form_TicTacToe {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.ticTacToeTitle_label = new System.Windows.Forms.Label();
+            this.button_topLeft = new System.Windows.Forms.Button();
+            this.button_topMiddle = new System.Windows.Forms.Button();
+            this.button_topRight = new System.Windows.Forms.Button();
+            this.button_centerLeft = new System.Windows.Forms.Button();
+            this.button_centerMiddle = new System.Windows.Forms.Button();
+            this.button_centerRight = new System.Windows.Forms.Button();
+            this.button_bottomLeft = new System.Windows.Forms.Button();
+            this.button_bottomMiddle = new System.Windows.Forms.Button();
+            this.button_bottomRight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ticTacToeTitle_label
+            // 
+            this.ticTacToeTitle_label.Font = new System.Drawing.Font("Miriam Mono CLM", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.ticTacToeTitle_label.ForeColor = System.Drawing.Color.Plum;
+            this.ticTacToeTitle_label.Location = new System.Drawing.Point(12, 9);
+            this.ticTacToeTitle_label.Name = "ticTacToeTitle_label";
+            this.ticTacToeTitle_label.Size = new System.Drawing.Size(319, 41);
+            this.ticTacToeTitle_label.TabIndex = 0;
+            this.ticTacToeTitle_label.Text = "Tic Tac Toe";
+            this.ticTacToeTitle_label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button_topLeft
+            // 
+            this.button_topLeft.BackColor = System.Drawing.Color.Plum;
+            this.button_topLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_topLeft.Font = new System.Drawing.Font("MingLiU-ExtB", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_topLeft.Location = new System.Drawing.Point(12, 68);
+            this.button_topLeft.Name = "button_topLeft";
+            this.button_topLeft.Size = new System.Drawing.Size(103, 103);
+            this.button_topLeft.TabIndex = 1;
+            this.button_topLeft.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_topLeft.UseVisualStyleBackColor = false;
+            this.button_topLeft.Click += new System.EventHandler(this.button_topLeft_Click);
+            // 
+            // button_topMiddle
+            // 
+            this.button_topMiddle.BackColor = System.Drawing.Color.Plum;
+            this.button_topMiddle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_topMiddle.Font = new System.Drawing.Font("MingLiU-ExtB", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_topMiddle.Location = new System.Drawing.Point(121, 68);
+            this.button_topMiddle.Name = "button_topMiddle";
+            this.button_topMiddle.Size = new System.Drawing.Size(103, 103);
+            this.button_topMiddle.TabIndex = 2;
+            this.button_topMiddle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_topMiddle.UseVisualStyleBackColor = false;
+            this.button_topMiddle.Click += new System.EventHandler(this.button_topMiddle_Click);
+            // 
+            // button_topRight
+            // 
+            this.button_topRight.BackColor = System.Drawing.Color.Plum;
+            this.button_topRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_topRight.Font = new System.Drawing.Font("MingLiU-ExtB", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_topRight.Location = new System.Drawing.Point(228, 68);
+            this.button_topRight.Name = "button_topRight";
+            this.button_topRight.Size = new System.Drawing.Size(103, 103);
+            this.button_topRight.TabIndex = 3;
+            this.button_topRight.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_topRight.UseVisualStyleBackColor = false;
+            this.button_topRight.Click += new System.EventHandler(this.button_topRight_Click);
+            // 
+            // button_centerLeft
+            // 
+            this.button_centerLeft.BackColor = System.Drawing.Color.Plum;
+            this.button_centerLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_centerLeft.Font = new System.Drawing.Font("MingLiU-ExtB", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_centerLeft.Location = new System.Drawing.Point(12, 177);
+            this.button_centerLeft.Name = "button_centerLeft";
+            this.button_centerLeft.Size = new System.Drawing.Size(103, 103);
+            this.button_centerLeft.TabIndex = 4;
+            this.button_centerLeft.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_centerLeft.UseVisualStyleBackColor = false;
+            this.button_centerLeft.Click += new System.EventHandler(this.button_centerLeft_Click);
+            // 
+            // button_centerMiddle
+            // 
+            this.button_centerMiddle.BackColor = System.Drawing.Color.Plum;
+            this.button_centerMiddle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_centerMiddle.Font = new System.Drawing.Font("MingLiU-ExtB", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_centerMiddle.Location = new System.Drawing.Point(121, 177);
+            this.button_centerMiddle.Name = "button_centerMiddle";
+            this.button_centerMiddle.Size = new System.Drawing.Size(103, 103);
+            this.button_centerMiddle.TabIndex = 5;
+            this.button_centerMiddle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_centerMiddle.UseVisualStyleBackColor = false;
+            this.button_centerMiddle.Click += new System.EventHandler(this.button_centerMiddle_Click);
+            // 
+            // button_centerRight
+            // 
+            this.button_centerRight.BackColor = System.Drawing.Color.Plum;
+            this.button_centerRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_centerRight.Font = new System.Drawing.Font("MingLiU-ExtB", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_centerRight.Location = new System.Drawing.Point(228, 177);
+            this.button_centerRight.Name = "button_centerRight";
+            this.button_centerRight.Size = new System.Drawing.Size(103, 103);
+            this.button_centerRight.TabIndex = 6;
+            this.button_centerRight.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_centerRight.UseVisualStyleBackColor = false;
+            this.button_centerRight.Click += new System.EventHandler(this.button_centerRight_Click);
+            // 
+            // button_bottomLeft
+            // 
+            this.button_bottomLeft.BackColor = System.Drawing.Color.Plum;
+            this.button_bottomLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_bottomLeft.Font = new System.Drawing.Font("MingLiU-ExtB", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_bottomLeft.Location = new System.Drawing.Point(12, 286);
+            this.button_bottomLeft.Name = "button_bottomLeft";
+            this.button_bottomLeft.Size = new System.Drawing.Size(103, 103);
+            this.button_bottomLeft.TabIndex = 7;
+            this.button_bottomLeft.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_bottomLeft.UseVisualStyleBackColor = false;
+            this.button_bottomLeft.Click += new System.EventHandler(this.button_bottomLeft_Click);
+            // 
+            // button_bottomMiddle
+            // 
+            this.button_bottomMiddle.BackColor = System.Drawing.Color.Plum;
+            this.button_bottomMiddle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_bottomMiddle.Font = new System.Drawing.Font("MingLiU-ExtB", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_bottomMiddle.Location = new System.Drawing.Point(121, 286);
+            this.button_bottomMiddle.Name = "button_bottomMiddle";
+            this.button_bottomMiddle.Size = new System.Drawing.Size(103, 103);
+            this.button_bottomMiddle.TabIndex = 8;
+            this.button_bottomMiddle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_bottomMiddle.UseVisualStyleBackColor = false;
+            this.button_bottomMiddle.Click += new System.EventHandler(this.button_bottomMiddle_Click);
+            // 
+            // button_bottomRight
+            // 
+            this.button_bottomRight.BackColor = System.Drawing.Color.Plum;
+            this.button_bottomRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_bottomRight.Font = new System.Drawing.Font("MingLiU-ExtB", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_bottomRight.Location = new System.Drawing.Point(228, 286);
+            this.button_bottomRight.Name = "button_bottomRight";
+            this.button_bottomRight.Size = new System.Drawing.Size(103, 103);
+            this.button_bottomRight.TabIndex = 9;
+            this.button_bottomRight.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_bottomRight.UseVisualStyleBackColor = false;
+            this.button_bottomRight.Click += new System.EventHandler(this.button_bottomRight_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.ClientSize = new System.Drawing.Size(356, 405);
+            this.Controls.Add(this.button_bottomRight);
+            this.Controls.Add(this.button_bottomMiddle);
+            this.Controls.Add(this.button_bottomLeft);
+            this.Controls.Add(this.button_centerRight);
+            this.Controls.Add(this.button_centerMiddle);
+            this.Controls.Add(this.button_centerLeft);
+            this.Controls.Add(this.button_topRight);
+            this.Controls.Add(this.button_topMiddle);
+            this.Controls.Add(this.button_topLeft);
+            this.Controls.Add(this.ticTacToeTitle_label);
+            this.Name = "MainForm";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button_topMiddle;
+        private System.Windows.Forms.Button button_topRight;
+        private System.Windows.Forms.Button button_centerLeft;
+        private System.Windows.Forms.Button button_centerMiddle;
+        private System.Windows.Forms.Button button_centerRight;
+        private System.Windows.Forms.Button button_bottomLeft;
+        private System.Windows.Forms.Button button_bottomMiddle;
+        private System.Windows.Forms.Button button_bottomRight;
+
+        private System.Windows.Forms.Button button_topLeft;
+
+        private System.Windows.Forms.Label ticTacToeTitle_label;
+
+        #endregion
+    }
+}
---
+++ Form_TicTacToe/MainForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_TicTacToe {
+    public partial class MainForm : Form {
+        private bool _turn = true;
+        private int _turnCount = 0;
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, EventArgs e) {
+            try {
+                Button button = (Button)sender;
+                button.Text = _turn == true ? "X" : "O";
+                _turn = !_turn;
+                _turnCount++;
+                button.Enabled = false;
+                Check();
+            }
+            catch {
+                MessageBox.Show(@"ERROR!", @"Error", MessageBoxButtons.AbortRetryIgnore);
+            }
+        }
+
+        private void Restart() {
+            foreach (Button button in Controls.OfType<Button>()) {
+                button.Enabled = true;
+                button.Text = "";
+            }
+
+            _turnCount = 0;
+            _turn = true;
+        }
+        
+        private void Check() {
+            bool thereIsAWinner = false;
+            
+            if (button_topLeft.Text == button_topMiddle.Text && button_topMiddle.Text == button_topRight.Text &&
+                button_topLeft.Text != "") {
+                thereIsAWinner = true;
+            } else if (button_centerLeft.Text == button_centerMiddle.Text &&
+                       button_centerMiddle.Text == button_centerRight.Text && button_centerLeft.Text != "") {
+                thereIsAWinner = true;
+            } else if (button_bottomLeft.Text == button_bottomMiddle.Text &&
+                       button_bottomMiddle.Text == button_bottomRight.Text && button_bottomLeft.Text != "") {
+                thereIsAWinner = true;
+            }
+
+            if (button_topLeft.Text == button_centerLeft.Text && button_centerLeft.Text == button_bottomLeft.Text && button_topLeft.Text != "") {
+                thereIsAWinner = true;
+            } else if (button_topMiddle.Text == button_centerMiddle.Text && button_centerMiddle.Text == button_bottomMiddle.Text && button_topMiddle.Text != "") {
+                thereIsAWinner = true;
+            } else if (button_topRight.Text == button_centerRight.Text && button_centerRight.Text == button_bottomRight.Text && button_topRight.Text != "") {
+                thereIsAWinner = true;
+            }
+
+            if (button_topLeft.Text == button_centerMiddle.Text && button_centerMiddle.Text == button_bottomRight.Text && button_topLeft.Text != "") {
+                thereIsAWinner = true;
+            } else if (button_topRight.Text == button_centerMiddle.Text && button_centerMiddle.Text == button_bottomLeft.Text && button_topRight.Text != "") {
+                thereIsAWinner = true;
+            }
+
+            if (thereIsAWinner) {
+                string winner = "";
+                if (_turn) {
+                    winner = "O";
+                }
+                else {
+                    winner = "X";
+                }
+
+                MessageBox.Show($@"The winner is {winner}! Click OK to restart the game.");
+                Restart();
+            }
+
+            
+            
+        }
+
+        #region Empty Button Click Event Methods
+        private void button_topLeft_Click(object sender, EventArgs e) {
+            OnButtonClick(sender, e);
+        }
+        private void button_topMiddle_Click(object sender, EventArgs e) {
+            OnButtonClick(sender, e);
+        }
+        private void button_topRight_Click(object sender, EventArgs e) {
+            OnButtonClick(sender, e);
+        }
+        private void button_centerLeft_Click(object sender, EventArgs e) {
+            OnButtonClick(sender, e);
+        }
+        private void button_centerMiddle_Click(object sender, EventArgs e) {
+            OnButtonClick(sender, e);
+        }
+        private void button_centerRight_Click(object sender, EventArgs e) {
+            OnButtonClick(sender, e);
+        }
+        private void button_bottomLeft_Click(object sender, EventArgs e) {
+            OnButtonClick(sender, e);
+        }
+        private void button_bottomMiddle_Click(object sender, EventArgs e) {
+            OnButtonClick(sender, e);
+        }
+        private void button_bottomRight_Click(object sender, EventArgs e) {
+            OnButtonClick(sender, e);
+        }
+        #endregion
+    }
+    
+}
